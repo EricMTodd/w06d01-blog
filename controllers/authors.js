029a9cb8ef0fafba0b51c8fff1adc9d05efc78e4
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-07-16 11:54:47 -0600

    Created update route.

--- controllers/authors.js
+++ controllers/authors.js
@@ -41,9 +41,13 @@ router.post("/", (req, res) => {
 // Show Route
 router.get("/:id", (req, res) => {
 	Author.findById(req.params.id, (err, shownAuthor) => {
-		res.render("authors/show.ejs", {
+		if (err) {
+			console.log(err, "Failed to show author.");
+		} else {
+			res.render("authors/show.ejs", {
 			"author": shownAuthor
-		});
+			});	
+		}
 	});
 });
 
@@ -54,6 +58,31 @@ router.delete("/:id", (req, res) => {
 		if (err) {
 			console.log(err, "Failed to delete author.");
 		}	else {
+			res.redirect("/authors");
+		}
+	})
+});
+
+
+// Edit Route
+router.get ("/:id/edit", (req, res) => {
+	Author.findById(req.params.id, (err, foundAuthor) => {
+		if (err) {
+			console.log(err, "Failed to find author.");
+		} else {
+			res.render("authors/edit.ejs", {
+			"author": foundAuthor
+		})
+		}
+	});
+});
+
+router.put("/:id", (req, res) => {
+	Author.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedAuthor) => {
+		if (err) {
+			console.log(err, "Failed to update model.");
+		} else {
+			console.log(updatedAuthor, "Model successfully updated.");
 			res.redirect("/authors");
 		}
 	})
